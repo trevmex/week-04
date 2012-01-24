fa5e7034dab09068ae3f60df6a6205bba0bc7de4
Trevor Lalish-Menagh <trev@trevreport.org>
2012-01-24 13:01:21 -0500

    Fixed superheroes

--- lib/display_superheroes.rb
+++ lib/display_superheroes.rb
@@ -8,3 +8,21 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+
+def display_superheroes(superhero, some_more_superheroes = nil)
+  hero_hash = {}
+  
+  [superhero, some_more_superheroes].each do |hero|
+    if hero.is_a? Array
+      hero.each do |supes|
+        hero_hash[supes.name] = supes
+      end
+    else
+      hero_hash[hero.name] = hero
+    end unless hero.nil?
+  end
+  
+  hero_hash.each_value do |hero|
+    puts hero
+  end
+end
