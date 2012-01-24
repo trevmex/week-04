58bb67e750a0f555e33f5b7d2004addab9796bf3
Trevor Lalish-Menagh <trev@trevreport.org>
2012-01-24 11:49:36 -0500

    Added first version of display superheroes solution

--- lib/display_superheroes.rb
+++ lib/display_superheroes.rb
@@ -8,3 +8,20 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+
+def display_superheroes(superheroes, more_superheroes = nil)
+  superheroes_hash = {}
+  [superheroes, more_superheroes].each do |supes|
+    if supes.is_a? Array
+      supes.each do |superhero|
+        superheroes_hash[superhero.name] = superhero
+      end
+    else
+      superhero = supes
+      superheroes_hash[superhero.name] = superhero
+    end unless supes.nil?
+  end
+  superheroes_hash.each_value do |supes|
+    puts supes
+  end
+end
