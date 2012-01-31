83230ba84e49fdcc529a259f2ac61092f774bf84
Trevor Lalish-Menagh <trev@trevreport.org>
2012-01-31 10:54:26 -0500

    Added url builder solution

--- lib/url_builder.rb
+++ lib/url_builder.rb
@@ -10,3 +10,13 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+
+def query_param_builder(params)
+  params.map do |key, value|
+    "#{key}=#{value}" unless value.nil?
+  end.compact.join("&")
+end
+
+def url_builder(url, params = {:results => '10'})
+  "#{url}?#{query_param_builder(params)}"
+end
